79b587e15fed05e2cbef326a0e9de61cdb88ba59
samuel-iseli <>
2021-02-16 09:05:13 +0100

    split settings into base, develop, production

---
+++ depotcommun/settings/production.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'depot-commun',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+DEBUG = False
+
+import django_heroku
+django_heroku.settings(locals())
