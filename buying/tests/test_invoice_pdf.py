f6afe777cd913dea69fdd01592b5eec4e99f1004
samuel-iseli <>
2022-01-06 20:20:53 +0100

    added tests and admin command for creating invoice pdf

--- buying/tests/test_invoice_pdf.py
+++ buying/tests/test_invoice_pdf.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 
 from buying.tests.test_invoice import InvoiceTestBase
-from buying.models import ItemPurchase
+from buying.models import Purchase
 from buying.billing import create_invoice
 from buying.invoice_pdf import InvoicePdfRenderer
 
@@ -12,18 +12,19 @@ class InvoicePdfTest(InvoiceTestBase):
         total_amount = 1.20 + 2 * 4.5
 
         # 1x item0, 2x item1
-        ip1 = ItemPurchase(
-            item=self.items[0], quantity=1, price=self.items[0].price, 
-            depot=self.depot, user=self.user)
+        ip1 = Purchase(
+            item=self.items[0], quantity=1,
+            price=self.items[0].price,
+            user=self.user)
         ip1.save()
         
-        ip2 = ItemPurchase(
+        ip2 = Purchase(
             item=self.items[1], quantity=2, price=self.items[1].price,
-            depot=self.depot, user=self.user)
+            user=self.user)
         ip2.save()
 
         invoice = create_invoice(
-            self.depot, self.user, self.invoice_datetime,
+            self.user, self.invoice_datetime,
             [ip1, ip2]
         )
         # test pdf rendering
