7b2af585fb45e8e26598d665aaec969e27607566
samuel-iseli <>
2022-01-08 19:43:17 +0100

    added root url redirect to admin

--- depotcommun/urls.py
+++ depotcommun/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    # entry path redirects to admin
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+    # commented out frontend index.html
+    # path('', TemplateView.as_view(template_name='index.html')),
     path('store/', include('store.urls')),
     path('admin/', admin.site.urls),
 ]
