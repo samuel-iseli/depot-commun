61e6daa6a735e17ae353390e63472b3592b6fa83
samuel-iseli <>
2022-01-23 00:53:51 +0100

    removed cookie samesite settings in develop settings

--- depotcommun/settings/develop.py
+++ depotcommun/settings/develop.py
@@ -16,14 +16,26 @@ DATABASES = {
     }
 }
 
-# disable cookie samesite config for react debugging
-SESSION_COOKIE_SAMESITE = 'None'
-# CSRF_COOKIE_SAMESITE = 'None'
 CORS_ALLOW_CREDENTIALS = True
 
 # redirect to react debug server after login
-LOGIN_REDIRECT_URL = 'http://localhost:3000/'
+# LOGIN_REDIRECT_URL = 'http://localhost:3000/'
 
 # e-mail test backend with files
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'mail-output')
+
+# logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
