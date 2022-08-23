18316f2e93a785ee3a95489fceca4e1c2b465486
samuel-iseli <>
2022-08-23 22:42:32 +0200

    added logging config to production settings

--- depotcommun/settings/production.py
+++ depotcommun/settings/production.py
@@ -22,5 +22,46 @@ SECURE_SSL_REDIRECT = True
 # EMAIL password from heroku settings
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 
+# this is similar to the logging config in django_heroku but
+# adds config for the 'django' logger
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+# apply heroku config
+# with logging=False our own logging configuration is used
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
