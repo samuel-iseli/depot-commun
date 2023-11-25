6928da3a6a726b8444ada128fe79875be9f5d199
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2023-11-25 20:56:10 +0100

    add root dir to python path

--- depotcommun/wsgi.py
+++ depotcommun/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import pathlib
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'depotcommun.settings.production')
+
+# add parentdir of depotcommun to path to allow import of depotcommun
+parentdir = pathlib.Path(__file__).parent.parent
+sys.path.append(parentdir)
 
 application = get_wsgi_application()
