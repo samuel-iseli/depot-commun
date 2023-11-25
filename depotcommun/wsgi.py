176168f0aa036d0c0f3c439b8dfba299c007e3f4
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2023-11-25 21:02:39 +0100

    add another dir to sys.path...

--- depotcommun/wsgi.py
+++ depotcommun/wsgi.py
@@ -10,13 +10,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 import sys
 import pathlib
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'depotcommun.settings.production')
 
 # add parentdir of depotcommun to path to allow import of depotcommun
-parentdir = pathlib.Path(__file__).parent.parent
+parentdir = str(pathlib.Path(__file__).parent)
+logging.error("added %s to sys.path" % parentdir)
 sys.path.append(parentdir)
 
 application = get_wsgi_application()
