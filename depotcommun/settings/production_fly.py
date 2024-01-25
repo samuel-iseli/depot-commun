738fff43889dc927577b0ff042527e3959f6cc2c
samuel-iseli <>
2024-01-25 10:29:15 +0100

    added csrf_trusted_origins and email_host_password settings for fly

--- depotcommun/settings/production_fly.py
+++ depotcommun/settings/production_fly.py
@@ -8,6 +8,9 @@ DATABASES = {
 
 APP_NAME = os.environ.get("FLY_APP_NAME")
 ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"] 
+CSRF_TRUSTED_ORIGINS = [f"https://{APP_NAME}.fly.dev"]
+
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
