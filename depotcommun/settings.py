25c8b3c1c96751c407d6e78934fdd7b23032bd4f
samuel-iseli <>
2024-01-26 22:39:28 +0100

    added jelastic domain to allowed_hosts

--- depotcommun/settings.py
+++ depotcommun/settings.py
@@ -132,6 +132,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.jcloud.ik-server.com']
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
