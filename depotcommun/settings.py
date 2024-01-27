96575a5a82f703042dd1d9e639016bb54e6c9386
samuel-iseli <>
2024-01-27 16:12:14 +0100

    rename STATIC_ROOT folder to static for jelastic compatibility

--- depotcommun/settings.py
+++ depotcommun/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # not used at them moment...
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'buying-frontend/build/static')
