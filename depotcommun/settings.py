70eb82c6c0087495326234782e0e4badaf511643
samuel-iseli <>
2024-01-27 15:54:03 +0100

    added hardcoded CSRF_TRUSTED_ORIGIN for jelastic

--- depotcommun/settings.py
+++ depotcommun/settings.py
@@ -143,11 +143,7 @@ REST_FRAMEWORK = {
 }
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.jcloud.ik-server.com']
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+CSRF_TRUSTED_ORIGINS = ["https://depotcommun.jcloud.ik-server.com"]
 
 # redirect to frontend after login
 LOGIN_REDIRECT_URL = '/'
