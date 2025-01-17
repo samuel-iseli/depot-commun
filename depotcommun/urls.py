fad619b690ad21387a2df0722c2fe0b64e895c32
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2025-01-17 17:40:57 +0100

    added django ninja rest api basics

--- depotcommun/urls.py
+++ depotcommun/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 from . import views
+from .api import api
 
 # login dialog using the template from rest_framework
 # must to be registered with app_name "rest_framework"
@@ -21,5 +22,6 @@ urlpatterns = [
     # path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('invoicepdf/<id>', views.invoice_pdf, name='invoice-pdf'),
-    path('articlespdf', views.articles_pdf, name='articles-pdf')
+    path('articlespdf', views.articles_pdf, name='articles-pdf'),
+    path("api/", api.urls),
 ]
